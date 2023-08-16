1b9a1e8e02d14d8b54c51362f139cce047f35e05
AliReza7222 <ali.r.salehi99@gmail.com>
2023-08-16 11:16:03 +0430

    create custom user and settings write based environ file

--- TheCinema/settings.py
+++ TheCinema/settings.py
@@ -10,22 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@gd*7da+1=1j_ue5*3b-v%$%_^+8_f#$_4nw0xryal&+ed(h-i'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
+
+# Config Custom Model User
+AUTH_USER_MODEL = "accounts.UserSite"
 
 
 # Application definition
