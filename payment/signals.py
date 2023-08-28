1921ecb7a1846ee4dc16ccdaac98b083ba6305cc
AliReza7222 <ali.r.salehi99@gmail.com>
2023-08-28 11:10:08 +0430

    complete view TransactionsView and create a signal for create payment for any Transactions

--- payment/signals.py
+++ payment/signals.py
@@ -6,11 +6,21 @@ from .models import Transactions, Payment
 
 @receiver(post_save, sender=Transactions)
 def create_payment(sender, instance, created, **kwargs):
+    movie = instance.movie_id
     if created:
+        # create a payment wit status 201
         data = {
             "status": 201,
             'transaction_id': instance,
-            'movie_id': instance.movie_id,
-            'amount': instance.amount
+            'movie_id': movie,
+            'amount': instance.amount,
+            'date_payment': instance.datetime_transactions
         }
         Payment.objects.create(**data)
+        # client user registered in room movie .
+        room = movie.room_movie
+        if room.quantity_person > movie.registered_ticket:
+            movie.registered_ticket += 1
+            if room.quantity_person == movie.registered_ticket:
+                movie.permission_sell = False
+            movie.save()
--- payment/views.py
+++ payment/views.py
@@ -4,23 +4,35 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import TransactionsSerializer
-from movie.models import Movie
+from .models import Payment, Transactions
+from .permissions import IsPermissionRegisterTicket
+from movie.models import Movie, TicketMovie
+from movie.utils import encode_data
 
 
 class TransactionsView(CreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPermissionRegisterTicket]
     serializer_class = TransactionsSerializer
+
+    def create_ticket_movie(self, user, movie, datetime_bought, transaction_id):
+        payment_id = Transactions.objects.get(id=transaction_id).payment.id
+        data_ticket = {
+            'user_client': user.phone_number,
+            'name_movie': movie.name_movie,
+            'datetime_bought': str(datetime_bought.strftime('%Y-%m-%d %H:%M:%S')),
+            'payment_id': str(payment_id)
+        }
+        data_encode, key = encode_data(data_ticket)
+        TicketMovie.objects.create(key_data=key, encode_data=data_encode, movie=movie, datetime_bought=datetime_bought)
+        return key
 
     def post(self, request, *args, **kwargs):
         data_post = request.data
         user = request.user
         movie_id = kwargs.get('movie_id')
-        movie = Movie.objects.filter(id=movie_id)
-        if not movie:
-            message = '! فیلمی پیدا نشد احتمالا در شناسایی فیلم مشکلی پیش اومده '
-            return Response({'error_message': message}, status=status.HTTP_404_NOT_FOUND)
+        movie = Movie.objects.get(id=movie_id)
         # if you are developing project set "link_back": 'http://127.0.0.1/home/'
-        amount = movie.first().price_ticket
+        amount = movie.price_ticket
         data = {
             'amount': amount,
             "link_back": 'http://127.0.0.1/home/',
@@ -28,7 +40,19 @@ class TransactionsView(CreateAPIView):
             "full_name_client": data_post.get('full_name_client', None)
         }
         serializer = self.get_serializer(data=data)
+
         if serializer.is_valid():
-            serializer.save(user=user, movie_id=movie.first())
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            obj_transaction = serializer.save(user=user, movie_id=movie)
+            key = self.create_ticket_movie(user=user, movie=movie,
+                                           datetime_bought=obj_transaction.datetime_transactions,
+                                           transaction_id=obj_transaction.id)
+            data_json = serializer.data
+            data_json['ticket_key'] = key
+            return Response(data=data_json, status=status.HTTP_201_CREATED)
+
+        # create a payment failure
+        Payment.objects.create(
+            user=user, movie_id=movie_id,
+            amount=amount, status=400
+        )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
